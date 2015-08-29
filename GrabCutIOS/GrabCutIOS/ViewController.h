98e1159fe04d66266341ceabaac908fb4de5cf98
Eucnhul Jeon <eunchul.jeon@navercorp.com>
2015-08-29 17:13:47 +0900

    First Commit

---
+++ GrabCutIOS/GrabCutIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GrabCutIOS
+//
+//  Created by EunchulJeon on 2015. 8. 29..
+//  Copyright (c) 2015년 EunchulJeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GrabCutIOS/GrabCutIOSTests/GrabCutIOSTests.m
@@ -0,0 +1,40 @@
+//
+//  GrabCutIOSTests.m
+//  GrabCutIOSTests
+//
+//  Created by EunchulJeon on 2015. 8. 29..
+//  Copyright (c) 2015년 EunchulJeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface GrabCutIOSTests : XCTestCase
+
+@end
+
+@implementation GrabCutIOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
